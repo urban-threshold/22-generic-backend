2443f545e82d7fd6cc5580a571b5fdd483b14e6c
spunkx <spunkx+tspinks@our.ecu.edu.au>
2022-08-19 23:04:58 +0800

    added fetch for an example dataset

--- src/routes/example.ts
+++ src/routes/example.ts
@@ -1,6 +1,6 @@
 import { IExampleType } from './../types';
 
-const allowLocalDevApp = "http://0.0.0.0:3000"
+const allowLocalDevApp = "http://localhost:3000"
 const allowCloudDevApp = ""
 
 const exampleData: IExampleType = {
@@ -8,9 +8,30 @@ const exampleData: IExampleType = {
     value: 1337
 }
 
+async function gatherResponse(response: any){
+    const { headers } = response;
+    const contentType = headers.get('content-type') || '';
+    if (contentType.includes('application/json')) {
+      return JSON.stringify(await response.json());
+    } else if (contentType.includes('application/text')) {
+      return response.text();
+    } else if (contentType.includes('text/html')) {
+      return response.text();
+    } else {
+      return response.text();
+    }
+}
+
 export default async function example(request: Request): Promise<Response> {
-    return new Response(exampleData.name, {
-        status: 204,
+    const init = {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      };
+      const response = await fetch('https://data.gov.au/data/dataset/138d9263-600f-4d35-993d-f1cec7ebcfdf/resource/ec9115f0-e762-4528-850b-ef7189ef18b4/download/city-of-casey.geojson', init);
+      const results = await gatherResponse(response);
+    return new Response(results, {
+        status: 200,
         headers: { 'Access-Control-Allow-Origin': allowLocalDevApp }
     })
 }
