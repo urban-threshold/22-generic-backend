b9a869225d481b386683be383b6a16c462328574
spunkx <spunkx+tspinks@our.ecu.edu.au>
2022-08-20 11:50:05 +0800

    added transport

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,12 @@
 import { Router } from 'itty-router'
 import example from './routes/example'
-import getFerry from './routes/transport'
+import { getFerry, getBusStops } from './routes/transport'
 
 
 const router = Router()
 
 router
+  .get("/busstops", getBusStops)
   .get("/ferries", getFerry)
   .get("/example", example)
   .options('/example', () => new Response("", {status: 204, headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET", "Access-Control-Allow-Headers": "*"}}))
--- src/routes/transport.ts
+++ src/routes/transport.ts
@@ -3,7 +3,7 @@ import gatherResponse from '../misc';
 const allowLocalDevApp = "http://localhost:3000"
 const allowCloudDevApp = ""
 
-export default async function getFerry(request: Request): Promise<Response> {
+async function getFerry(request: Request): Promise<Response> {
     const init = {
         headers: {
           'content-type': 'application/json;charset=UTF-8',
@@ -16,3 +16,20 @@ export default async function getFerry(request: Request): Promise<Response> {
         headers: { 'Access-Control-Allow-Origin': allowLocalDevApp }
     })
 }
+
+async function getBusStops(request: Request): Promise<Response> {
+    const init = {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      };
+      const response = await fetch('https://www.spatial-data.brisbane.qld.gov.au/datasets/9fa7ded987ac4b7085af4949034d0594_0.geojson?outSR=%7B%22latestWkid%22%3A28356%2C%22wkid%22%3A28356%7D', init);
+      const results = await gatherResponse(response);
+    return new Response(results, {
+        status: 200,
+        headers: { 'Access-Control-Allow-Origin': allowLocalDevApp }
+    })
+}
+
+
+export {getFerry, getBusStops}
